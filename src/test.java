928bb9a2f3a7bd2326a2ecc1d0edf554364ff831
whylwhyl <whyl@mail.ru>
2015-09-30 22:45:35 +0300

    create first test
    test project for fillform

---
+++ src/test.java
@@ -0,0 +1,99 @@
+public class test {
+    String name;
+    String applicantLastName;
+    String applicantFirstNameAndPatronymic;
+    String applicantСitizenship;
+    String applicantBirthDay;
+    String applicantBirthMonth;
+    String applicantBirthYear;
+    Boolean checkboxApplicantIsMale;
+    String applicantBirthCountry;
+    String applicantBirthLocality;
+    String applicantDocType;
+    String applicantDocSeries;
+    String applicantDocNumber;
+    String applicantDocEditionDay;
+    String applicantDocEditionMonth;
+    String applicantDocEditionYear;
+    /*
+    applicantDocValidityDay
+            applicantDocValidityMonth
+    applicantDocValidityYear
+            checkboxApplicantConfirmationDocTypeVisa
+    checkboxApplicantConfirmationDocTypeResidence
+            checkboxApplicantConfirmationDocTypeTemporaryResidencePermit
+    applicantConfirmationDocSeries
+            applicantConfirmationDocNumber
+    applicantConfirmationDocEditionDay
+            applicantConfirmationDocEditionMonth
+    applicantConfirmationDocEditionYear
+            applicantConfirmationDocValidityDay
+    applicantConfirmationDocValidityMonth
+            applicantConfirmationDocValidityYear
+    checkboxApplicantPurposeOfEntryOfficial
+            checkboxApplicantPurposeOfEntryTourism
+    checkboxApplicantPurposeOfEntryBusiness
+            checkboxApplicantPurposeOfEntryEducation
+    checkboxApplicantPurposeOfEntryWork
+            checkboxApplicantPurposeOfEntryPrivate
+    checkboxApplicantPurposeOfEntryTransit
+            checkboxApplicantPurposeOfEntryHumanitarian
+    checkboxApplicantPurposeOfEntryOther
+            applicantProfession
+    applicantEntryDay
+            applicantEntryMonth
+    applicantEntryYear
+            applicantTenureDay
+    applicantTenureMonth
+            applicantTenureYear
+    applicantMigrationCardSeries
+            applicantMigrationCardNumber
+    applicantLegalRepresentatives1
+            applicantLegalRepresentatives2
+    applicantLegalRepresentatives3
+            applicantLegalRepresentatives4
+    applicantLegalRepresentatives5
+    applicantResidenceRegion
+            applicantResidenceDistrict
+    applicantResidenceLocality
+            applicantResidenceStreet
+    applicantResidenceHouse
+            applicantResidencePavilion
+    applicantResidenceBuilding
+            applicantResidenceApartment
+            applicantResidenceTelephone
+    checkboxHostTypeOrganization
+            checkboxHostTypeIndividual
+    hostLastName
+            hostBirthDay
+    hostBirthMonth
+            hostBirthYear
+    hostFirstNameAndPatronymic
+            hostDocType
+    hostDocSeries
+            hostDocNumber
+    hostDocEditionDay
+            hostDocEditionMonth
+    hostDocEditionYear
+            hostDocValidityDay
+    hostDocValidityMonth
+            hostDocValidityYear
+    hostResidenceRegion
+            hostResidenceDistrict
+    hostResidenceLocality
+            hostResidenceStreet
+    hostResidenceHouse
+            hostResidencePavilion
+    hostResidenceBuilding
+            hostResidenceApartment
+    hostResidenceTelephone
+            hostOrganization1
+    hostOrganization2
+            hostOrganizationAddress1
+    hostOrganizationAddress2
+            hostINN
+    applicantDepartureDay
+            applicantDepartureMonth
+    applicantDepartureYear
+            */
+}
